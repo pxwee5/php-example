43edff3326c2e839b4bbf7f851937a6a606b3e1a
James Wee <jameswee@Jamess-MacBook-Pro.local>
2017-03-23 12:38:57 +1300

    Added functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Multimedia Theme functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Multimedia
+ */
+
+// Initialize Multimedia theme.
+require_once get_template_directory() . '/includes/init.php';
